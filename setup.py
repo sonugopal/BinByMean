94a6c8494c10e04d66f5fcb9c2ea6bc7b45820ae
sonugopal <sonugopal41@gmail.com>
2019-12-13 12:57:53 +0530

    keywords added

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author_email = 'sonu.g@mca.christuniversity.in',      # Type in your E-Mail
   url = 'https://github.com/sonugopal/BinByMean.git',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/sonugopal/BinByMean/archive/v0.1.tar.gz',    # I explain this later on
-  keywords = ['ZScore','Z Score','Z-Score', 'normalization', 'python'],   # Keywords that define your package best
+  keywords = ['Bin By Mean', 'Noise Reduction', 'python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'numpy',
