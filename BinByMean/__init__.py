5e4b84aea8828f4757fe5b993a23e8467f6cd255
sonugopal <sonugopal41@gmail.com>
2019-12-13 12:20:57 +0530

    Initial commit with program

---
+++ BinByMean/__init__.py
@@ -0,0 +1,28 @@
+import itertools
+import  pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
+
+def binByMean(data,binNo):
+    data=np.sort(data)
+    total=len(data)
+    binWeight=int(total/binNo)
+    bin=np.zeros((binNo,binWeight))
+    index=0
+    for i in range(binNo):
+        for j in range(binWeight):
+            bin[i,j]=data[index]
+            index+=1
+    result = np.zeros(binNo)
+
+    for i in range(binNo):
+        result[i] = np.mean(bin[i])
+
+    return result
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'BinByMean',         # How you named your package folder (MyLib)
+  packages = ['BinByMean'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Performs Bin By mean in your dataset.',   # Give a short description about your library
+  author = 'SONU GOPAL',                   # Type in your name
+  author_email = 'sonu.g@mca.christuniversity.in',      # Type in your E-Mail
+  url = 'https://github.com/sonugopal/BinByMean.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Alwin1847207/ZScore/archive/v0.1.tar.gz',    # I explain this later on
+  keywords = ['ZScore','Z Score','Z-Score', 'normalization', 'python'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'pandas',
+          'numpy',
+      'statistics',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
