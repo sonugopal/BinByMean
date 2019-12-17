1c16b0c90e17b7a6ae473e397edc1febe4077266
sonugopal <sonugopal41@gmail.com>
2019-12-17 15:01:12 +0530

    new dist added

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'BinByMean',         # How you named your package folder (MyLib)
   packages = ['BinByMean'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Performs Bin By mean in your dataset.',   # Give a short description about your library
   author = 'SONU GOPAL',                   # Type in your name
   author_email = 'sonu.g@mca.christuniversity.in',      # Type in your E-Mail
   url = 'https://github.com/sonugopal/BinByMean.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sonugopal/BinByMean/archive/v0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sonugopal/BinByMean/archive/v0.2.tar.gz',    # I explain this later on
   keywords = ['Bin By Mean', 'Noise Reduction', 'python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
