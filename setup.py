6e2f5f8b5c2462fedf750b5a4b1031e3866708a7
sonugopal <sonugopal41@gmail.com>
2019-12-20 09:44:05 +0530

    package name changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'binbymean',         # How you named your package folder (MyLib)
-  packages = ['binbymean'],   # Chose the same as "name"
+  name = 'bin_by_mean',         # How you named your package folder (MyLib)
+  packages = ['bin_by_mean'],   # Chose the same as "name"
   version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Performs Bin By mean in your dataset.',   # Give a short description about your library
