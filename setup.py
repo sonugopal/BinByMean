52e4b0c9ea749e1ee64defd8149ff7ac2b8a9434
sonugopal <sonugopal41@gmail.com>
2019-12-20 09:25:08 +0530

    dist deleted

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'SONU GOPAL',                   # Type in your name
   author_email = 'sonu.g@mca.christuniversity.in',      # Type in your E-Mail
   url = 'https://github.com/sonugopal/BinByMean.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/sonugopal/BinByMean/archive/v0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/sonugopal/binbyname/archive/0.2.tar.gz',    # I explain this later on
   keywords = ['Bin By Mean', 'Noise Reduction', 'python'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
